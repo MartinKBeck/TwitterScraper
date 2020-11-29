d664559a0bb8b8170d134a4adabdcb76a203a3d5
Martin Beck, M.S <53153026+MartinBeckUT@users.noreply.github.com>
2020-11-29 15:53:14 -0800

    Add files via upload

---
+++ snscrape/python-wrapper/snscrape-python-wrapper.py
@@ -0,0 +1,58 @@
+# Pip install the command below if you don't have the development version of snscrape 
+# !pip install git+https://github.com/JustAnotherArchivist/snscrape.git
+
+# Run the below command if you don't already have Pandas
+# !pip install pandas
+
+# Imports
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# Below are two ways of scraping using the Python Wrapper.
+# Comment or uncomment as you need. If you currently run the script as is it will scrape both queries
+# then output two different csv files.
+
+# Query by username
+# Setting variables to be used below
+maxTweets = 100
+
+# Creating list to append tweet data to
+tweets_list1 = []
+
+# Using TwitterSearchScraper to scrape data 
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:jack').get_items()):
+    if i>maxTweets:
+        break
+    tweets_list1.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+
+# Creating a dataframe from the tweets list above
+tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
+# Display first 5 entries from dataframe
+# tweets_df1.head()
+
+# Export dataframe into a CSV
+tweets_df1.to_csv('user-tweets.csv', sep=',', index=False)
+
+
+# Query by text search
+# Setting variables to be used below
+maxTweets = 500
+
+# Creating list to append tweet data to
+tweets_list2 = []
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('its the elephant since:2020-06-01 until:2020-07-31').get_items()):
+    if i>maxTweets:
+        break
+    tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+
+# Creating a dataframe from the tweets list above
+tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
+# Display first 5 entries from dataframe
+tweets_df2.head()
+
+# Export dataframe into a CSV
+tweets_df2.to_csv('text-query-tweets.csv', sep=',', index=False)
